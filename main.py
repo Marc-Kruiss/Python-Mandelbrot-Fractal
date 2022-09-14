e047d6d8abf8aace242606ebbccec4d6446cd69b
Marc Kruiß <k.marc@a1.net>
2022-09-14 18:43:06 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame as pg
+import numpy as np
+
+res = width, height = 160, 100
+
+
+class App:
+    def __init__(self):
+        self.screen = pg.display.set_mode(res, pg.SCALED)
+        self.clock = pg.time.Clock()
+
+    def run(self):
+        while True:
+            self.screen.fill('black')
+            pg.display.flip()
+
+            [exit() for i in pg.event.get() if i.type == pg.QUIT]
+            self.clock.tick()
+            pg.display.set_caption(f'FPS: {self.clock.get_fps()}')
+
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
