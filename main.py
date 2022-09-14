5e273f4e5d8dcf9e53d027ad694b76579f83877e
Marc Kruiß <k.marc@a1.net>
2022-09-14 18:46:27 +0200

    add fractal class

--- main.py
+++ main.py
@@ -4,6 +4,25 @@ import numpy as np
 res = width, height = 160, 100
 
 
+class Fractal:
+    def __init__(self):
+        self.app = app
+        self.screen_array = np.full((width, height, 3), [0, 0, 0], dtype=np.uint8)
+
+    def render(self):
+        pass
+
+    def update(self):
+        self.render()
+
+    def draw(self):
+        pg.surfarray.blit_array(self.app.screen, self.screen_array)
+
+    def run(self):
+        self.update()
+        self.draw()
+
+
 class App:
     def __init__(self):
         self.screen = pg.display.set_mode(res, pg.SCALED)
